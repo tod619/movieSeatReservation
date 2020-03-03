cf9bcd56b10d51658bdee69a3b4b1443dd4e5049
tod619 <trevorodwyer619@hotmail.com>
2020-03-03 10:55:58 +0000

    Update count of seats and total price using js in app.js

--- app.js
+++ app.js
@@ -5,4 +5,32 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
-//console.log(movieSelect);
+const ticketPrice = +movieSelect.value;
+
+// console.log(typeof ticketPrice);
+
+//Functions
+
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = ticketPrice * selectedSeatsCount;
+}
+// End of updateSelectedCount function
+
+// Add eventListenre to container
+container.addEventListener("click", e => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    // console.log(e.target);
+    e.target.classList.toggle("selected");
+
+    updateSelectedCount();
+  }
+});
