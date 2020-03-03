0397c51fdc5aa618aabd64042af1da5f1a962573
tod619 <trevorodwyer619@hotmail.com>
2020-03-03 10:25:38 +0000

    Added the const's that will be used in app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+// Constants
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+
+//console.log(movieSelect);
